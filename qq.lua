86d8d272432915b0add6116f13e72b2a2596c183
DownloadPerm <161867616+DownloadPerm@users.noreply.github.com>
2025-08-23 05:37:30 +0700

    Update qq.lua

--- qq.lua
+++ qq.lua
@@ -2,12 +2,39 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
 
-
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Silent Aim",
     Text = "Script successfully loaded!",
     Duration = 3
 })
+
+-- Discord
+local discordInvite = "https://discord.gg/n99CyYESPZ"
+
+wait(3) 
+
+local http_request = (syn and syn.request) or (http and http.request) or request
+if http_request then
+    http_request({
+        Url = "http://127.0.0.1:6463/rpc?v=1",
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json",
+            ["Origin"] = "https://discord.com"
+        },
+        Body = game:GetService("HttpService"):JSONEncode({
+            cmd = "INVITE_BROWSER",
+            args = {code = string.match(discordInvite, "discord%.gg/(%w+)")},
+            nonce = game:GetService("HttpService"):GenerateGUID(false)
+        })
+    })
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Executor Not Supported",
+        Text = "Join manually: "..discordInvite,
+        Duration = 5
+    })
+end
 
 -- Silent Aim
 function enableSilentAim()
